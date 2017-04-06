eba5f67fa6c1005bc8f9977a35352eaf3706ca46
Sergio Oller <sergioller@gmail.com>
2017-04-06 17:30:20 +0200

    Use public domain replacements for used POSIX functions

--- src/utils/PosixCompat.c
+++ src/utils/PosixCompat.c
@@ -1,33 +1,9 @@
-#include "PosixCompat.h"
+/* coding: utf-8 */
+/* No copyright is applicable */
+/* The functions here have been released under public domain */
+/* These functions are only used if the system does not already provide them */
 
-/*
- * Copyright (c) 1988, 1993
- *	The Regents of the University of California.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
+#include "PosixCompat.h"
 
 #if !HAVE_STRDUP
 #include <string.h>
@@ -35,186 +11,151 @@
 
 char *strdup(const char *str)
 {
-    size_t len;
-    char *copy = NULL;
-
-    len = strlen(str);
-    copy = malloc(len + 1);
-    if (copy != NULL)
-        memcpy(copy, str, len + 1);
-    return (copy);
+    char *str_copy = malloc(strlen(str)+1);
+    if (str_copy == NULL) return NULL;
+    strcpy(str_copy, str);
+    return str_copy;
 }
 #endif
-
 
 #if !HAVE_GETOPT
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
-
-int opterr = 1,                 /* if error message should be printed */
-    optind = 1,                 /* index into parent argv vector */
-    optopt,                     /* character checked for validity */
-    optreset;                   /* reset getopt */
-char *optarg;                   /* argument associated with option */
-
-#define	BADCH	(int)'?'
-#define	BADARG	(int)':'
-#define	EMSG	""
-
 /*
- * getopt --
- *	Parse argc/argv argument vector.
+ * This is a version of the public domain getopt implementation by
+ * Henry Spencer originally posted to net.sources.
+ *
+ * This is in the public domain.
  */
-int getopt(int nargc, char *const *nargv, const char *ostr)
-{
-    static char *place = EMSG;  /* option letter processing */
-    char *oli;                  /* option letter list index */
 
-    if (optreset || !*place)
-    {                           /* update scanning pointer */
-        optreset = 0;
-        if (optind >= nargc || *(place = nargv[optind]) != '-')
-        {
-            place = EMSG;
-            return (-1);
-        }
-        if (place[1] && *++place == '-')
-        {                       /* found "--" */
-            ++optind;
-            place = EMSG;
-            return (-1);
-        }
-    }                           /* option letter okay? */
-    if ((optopt = (int) *place++) == (int) ':' ||
-        !(oli = strchr(ostr, optopt)))
-    {
-        /*
-         * if the user didn't specify '-' as an option,
-         * assume it means -1.
-         */
-        if (optopt == (int) '-')
-            return (-1);
-        if (!*place)
-            ++optind;
-        if (opterr && *ostr != ':')
-            (void) printf("illegal option -- %c\n", optopt);
-        return (BADCH);
-    }
-    if (*++oli != ':')
-    {                           /* don't need argument */
-        optarg = NULL;
-        if (!*place)
-            ++optind;
-    }
-    else
-    {                           /* need an argument */
-        if (*place)             /* no white space */
-            optarg = place;
-        else if (nargc <= ++optind)
-        {                       /* no arg */
-            place = EMSG;
-            if (*ostr == ':')
-                return (BADARG);
-            if (opterr)
-                (void) printf("option requires an argument -- %c\n", optopt);
-            return (BADCH);
-        }
-        else                    /* white space */
-            optarg = nargv[optind];
-        place = EMSG;
-        ++optind;
-    }
-    return (optopt);            /* dump back option letter */
+#include <stdio.h>
+#include <string.h>
+
+char *optarg; /* Global argument pointer. */
+int optind = 0; /* Global argv index. */
+
+int getopt(int argc, char *argv[], char *optstring)
+{
+        static char *scan = NULL; /* Private scan pointer. */
+	char c;
+	char *place;
+
+	optarg = NULL;
+
+	if (!scan || *scan == '\0') {
+		if (optind == 0)
+			optind++;
+
+		if (optind >= argc || argv[optind][0] != '-' || argv[optind][1] == '\0')
+			return EOF;
+		if (argv[optind][1] == '-' && argv[optind][2] == '\0') {
+			optind++;
+			return EOF;
+		}
+
+		scan = argv[optind]+1;
+		optind++;
+	}
+
+	c = *scan++;
+	place = strchr(optstring, c);
+
+	if (!place || c == ':') {
+		fprintf(stderr, "%s: unknown option -%c\n", argv[0], c);
+		return '?';
+	}
+
+	place++;
+	if (*place == ':') {
+		if (*scan != '\0') {
+			optarg = scan;
+			scan = NULL;
+		} else if( optind < argc ) {
+			optarg = argv[optind];
+			optind++;
+		} else {
+			fprintf(stderr, "%s: option requires argument -%c\n", argv[0], c);
+			return ':';
+		}
+	}
+
+	return c;
 }
 #endif
 
 
-
-
-
-/*	$NetBSD: fgetln.c,v 1.9 2008/04/29 06:53:03 martin Exp $	*/
-/*-
- * Copyright (c) 2011 The NetBSD Foundation, Inc.
- * All rights reserved.
- *
- * This code is derived from software contributed to The NetBSD Foundation
- * by Christos Zoulas.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE NETBSD FOUNDATION, INC. AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-
 #if !HAVE_GETLINE
+/* getline.c
+ *
+ * getdelim(), getline() - read a delimited record from stream, ersatz implementation
+ *
+ * For more details, see: http://pubs.opengroup.org/onlinepubs/9699919799/functions/getline.html
+ *
+ * This implementation of getline was released into the public domain at 
+ * https://github.com/ivanra/getline
+ *
+ *
+ */
 #include <stdlib.h>
-#include <stdio.h>
 #include <errno.h>
-#include <string.h>
+#include <limits.h>
 
-ssize_t getdelim(char **buf, size_t * bufsiz, int delimiter, FILE * fp)
-{
-    char *ptr, *eptr;
+ssize_t getdelim(char **lineptr, size_t *n, int delim, FILE *stream) {
+    char c, *cur_pos, *new_lineptr;
+    size_t new_lineptr_len;
 
+    if (lineptr == NULL || n == NULL || stream == NULL) {
+        errno = EINVAL;
+        return -1;
+    }
 
-    if (*buf == NULL || *bufsiz == 0)
-    {
-        *bufsiz = 8192;
-        if ((*buf = malloc(*bufsiz)) == NULL)
+    if (*lineptr == NULL) {
+        *n = 128; /* init len */
+        if ((*lineptr = (char *)malloc(*n)) == NULL) {
+            errno = ENOMEM;
             return -1;
+        }
     }
 
-    for (ptr = *buf, eptr = *buf + *bufsiz;;)
-    {
-        int c = fgetc(fp);
-        if (c == -1)
-        {
-            if (feof(fp))
-                return ptr == *buf ? -1 : ptr - *buf;
-            else
+    cur_pos = *lineptr;
+    for (;;) {
+        c = getc(stream);
+
+        if (ferror(stream) || (c == EOF && cur_pos == *lineptr))
+            return -1;
+
+        if (c == EOF)
+            break;
+
+        if ((*lineptr + *n - cur_pos) < 2) {
+            if (SSIZE_MAX / 2 < *n) {
+#ifdef EOVERFLOW
+                errno = EOVERFLOW;
+#else
+                errno = ERANGE; /* no EOVERFLOW defined */
+#endif
                 return -1;
-        }
-        *ptr++ = c;
-        if (c == delimiter)
-        {
-            *ptr = '\0';
-            return ptr - *buf;
-        }
-        if (ptr + 2 >= eptr)
-        {
-            char *nbuf;
-            size_t nbufsiz = *bufsiz * 2;
-            ssize_t d = ptr - *buf;
-            if ((nbuf = realloc(*buf, nbufsiz)) == NULL)
+            }
+            new_lineptr_len = *n * 2;
+
+            if ((new_lineptr = (char *)realloc(*lineptr, new_lineptr_len)) == NULL) {
+                errno = ENOMEM;
                 return -1;
-            *buf = nbuf;
-            *bufsiz = nbufsiz;
-            eptr = nbuf + nbufsiz;
-            ptr = nbuf + d;
+            }
+            *lineptr = new_lineptr;
+            *n = new_lineptr_len;
         }
+
+        *cur_pos++ = c;
+
+        if (c == delim)
+            break;
     }
+
+    *cur_pos = '\0';
+    return (ssize_t)(cur_pos - *lineptr);
 }
 
-ssize_t getline(char **buf, size_t * bufsiz, FILE * fp)
-{
-    return getdelim(buf, bufsiz, '\n', fp);
+ssize_t getline(char **lineptr, size_t *n, FILE *stream) {
+    return getdelim(lineptr, n, '\n', stream);
 }
 
 #endif
--- src/utils/PosixCompat.h
+++ src/utils/PosixCompat.h
@@ -1,31 +1,23 @@
-/*
- * Copyright (c) 1988, 1993
- *	The Regents of the University of California.  All rights reserved.
+/* coding: utf-8 */
+/* Saga - Un transcriptor fonético para el idioma español
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
+ * Copyright (C) 1993-2009  Albino Nogueiras Rodríguez y José B. Mariño
+ *       TALP - Universitat Politècnica de Catalunya, ESPAÑA
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 #include "config.h"
 
@@ -53,7 +45,15 @@ extern int optind, opterr, optopt;
 
 #if !HAVE_DECL_GETLINE
 #include <stdio.h>
-ssize_t getline(char **buf, size_t * bufsiz, FILE * fp);
+#include <stdint.h>
+
+#ifndef ssize_t
+#define ssize_t int64_t
+#endif
+#ifndef SSIZE_MAX
+#define SSIZE_MAX INT64_MAX
+#endif
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 #endif
 
 
